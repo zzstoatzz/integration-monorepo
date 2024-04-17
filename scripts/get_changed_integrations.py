271105557b4e9336370c48a35c37e57033d6b0fc
zzstoatzz <thrast36@gmail.com>
2024-04-17 13:50:26 -0500

    chore: wip

--- scripts/get_changed_integrations.py
+++ scripts/get_changed_integrations.py
@@ -3,7 +3,10 @@ import subprocess
 import sys
 from pathlib import Path
 
-INTEGRATIONS_PATH = "src/integrations"
+import toml
+
+INTEGRATIONS_BASEPATH = "src/integrations"
+INTEGRATIONS_VERSIONS_FILE = "src/integrations/versions.toml"
 
 
 def get_changed_files(previous_tag: str, current_commit: str) -> list[str]:
@@ -12,16 +15,37 @@ def get_changed_files(previous_tag: str, current_commit: str) -> list[str]:
     return output.strip().split("\n")
 
 
-def get_changed_integrations(changed_files: list[str], glob_pattern: str) -> list[str]:
-    integrations_base_path = Path(INTEGRATIONS_PATH)
-    changed_integrations = set()
+def get_next_version(integration_name: str, current_version: str) -> str:
+    cmd = f"cz bump --dry-run --increment-by 1 --file-name {INTEGRATIONS_VERSIONS_FILE} --key integrations.{integration_name}"
+    output = subprocess.check_output(cmd, shell=True, text=True)
+    return output.strip().split(" ")[-1]
 
-    for file_path in changed_files:
+
+def get_changed_integrations(
+    changed_files: list[str], glob_pattern: str
+) -> dict[str, str]:
+    integrations_base_path = Path(INTEGRATIONS_BASEPATH)
+    changed_integrations = {}
+    versions: dict[str, str] = toml.load(Path(INTEGRATIONS_VERSIONS_FILE)).get(
+        "integrations", {}
+    )
+
+    modified_integrations_files = [
+        file_path
+        for file_path in changed_files
+        if Path(file_path).match(glob_pattern)
+        and integrations_base_path in Path(file_path).parents
+    ]
+
+    for file_path in modified_integrations_files:
         path = Path(file_path)
-        if path.match(glob_pattern) and integrations_base_path in path.parents:
-            changed_integrations.add(path.parent.name)
+        integration_name = path.parent.name
+        if integration_name in versions:
+            changed_integrations[integration_name] = get_next_version(
+                integration_name, versions[integration_name]
+            )
 
-    return sorted(changed_integrations)
+    return changed_integrations
 
 
 def main(glob_pattern: str = "**.py"):
